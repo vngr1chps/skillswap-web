a6633805a79e9624f1841f82943de4a1c087794a
Maria-bog <mariia.bg234@gmail.com>
2025-12-16 14:00:41 +0300

    Удалила лишние библиотеки

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
 from django.contrib import messages
 from django.contrib.auth import logout
 
-User = get_user_model()
 @login_required
 def profile_view(request):
     user = request.user
